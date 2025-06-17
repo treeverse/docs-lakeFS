7a9055097a31459d5474f41a6ee763b1111fb954
Barak Amar <barak.amar@treeverse.io>
2025-06-17 10:39:01 +0300

    fix non mkdocs versions

--- v0.48/assets/js/copy-code.js
+++ v0.48/assets/js/copy-code.js
@@ -1,1 +1,18 @@
-../../../archive/assets/js/copy-code.js
+$(() => {
+    let copyCodeContainer = $("<div class=\"copy-code-container\">" +
+        "<button class=\"copy-code-button\" aria-label=\"Copy code block to your clipboard\">" +
+        "<i class=\"far fa-copy\"></i>" +
+        "</button></div>");
+    $("div.highlighter-rouge").prepend(copyCodeContainer);
+    $("div.highlighter-rouge .copy-code-button").click(function() {
+        const tempTextArea = document.createElement('textarea');
+        tempTextArea.textContent = $(this).parent().parent().find("code").text()
+        document.body.appendChild(tempTextArea);
+        const selection = document.getSelection();
+        selection.removeAllRanges();
+        tempTextArea.select();
+        document.execCommand('copy');
+        selection.removeAllRanges();
+        document.body.removeChild(tempTextArea);
+    });
+});
--- v0.48/assets/js/swagger-ui-lakefs.js
+++ v0.48/assets/js/swagger-ui-lakefs.js
@@ -1,1 +1,20 @@
-../../../archive/assets/js/swagger-ui-lakefs.js
+
+window.onload = function() {
+    // Begin Swagger UI call region
+    const ui = SwaggerUIBundle({
+        url: "../assets/js/swagger.yml",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        validatorUrl: null,
+        supportedSubmitMethods: [],
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [],
+        layout: "BaseLayout"
+    })
+    // End Swagger UI call region
+
+    window.ui = ui
+}
